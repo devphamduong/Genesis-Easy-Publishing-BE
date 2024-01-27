bc3a82d657756e94df37d9d66c34296dbcb64f29
baspdd <duypdhe160308@fpt.edu.vn>
2024-01-27 19:20:36 +0700

    up

--- app/Controllers/CategoriesController.cs
+++ app/Controllers/CategoriesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using app.Models;
 using NuGet.Common;
+using app.Service;
 
 namespace app.Controllers
 {
@@ -15,7 +16,7 @@ namespace app.Controllers
     public class CategoriesController : ControllerBase
     {
         private readonly EasyPublishingContext _context;
-
+        private MsgService _msgService = new MsgService();
         public CategoriesController(EasyPublishingContext context)
         {
             _context = context;
@@ -26,104 +27,96 @@ namespace app.Controllers
         public async Task<ActionResult> GetCategories()
         {
             var cate = await _context.Categories.ToListAsync();
-            return new JsonResult(new
-            {
-                EC = 0,
-                EM = "Category successfully",
-                DT = new
-                {
-                    category_list = cate
-                },
-            });
+            return _msgService.MsgReturn("Categories successfully", cate);
         }
 
-        // GET: api/Categories/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Category>> GetCategory(int id)
-        {
-            if (_context.Categories == null)
-            {
-                return NotFound();
-            }
-            var category = await _context.Categories.FindAsync(id);
+        //// GET: api/Categories/5
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<Category>> GetCategory(int id)
+        //{
+        //    if (_context.Categories == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    var category = await _context.Categories.FindAsync(id);
 
-            if (category == null)
-            {
-                return NotFound();
-            }
+        //    if (category == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return category;
-        }
+        //    return category;
+        //}
 
-        // PUT: api/Categories/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutCategory(int id, Category category)
-        {
-            if (id != category.CategoryId)
-            {
-                return BadRequest();
-            }
+        //// PUT: api/Categories/5
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutCategory(int id, Category category)
+        //{
+        //    if (id != category.CategoryId)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            _context.Entry(category).State = EntityState.Modified;
+        //    _context.Entry(category).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CategoryExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!CategoryExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            return NoContent();
-        }
+        //    return NoContent();
+        //}
 
-        // POST: api/Categories
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Category>> PostCategory(Category category)
-        {
-            if (_context.Categories == null)
-            {
-                return Problem("Entity set 'EasyPublishingContext.Categories'  is null.");
-            }
-            _context.Categories.Add(category);
-            await _context.SaveChangesAsync();
+        //// POST: api/Categories
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPost]
+        //public async Task<ActionResult<Category>> PostCategory(Category category)
+        //{
+        //    if (_context.Categories == null)
+        //    {
+        //        return Problem("Entity set 'EasyPublishingContext.Categories'  is null.");
+        //    }
+        //    _context.Categories.Add(category);
+        //    await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetCategory", new { id = category.CategoryId }, category);
-        }
+        //    return CreatedAtAction("GetCategory", new { id = category.CategoryId }, category);
+        //}
 
-        // DELETE: api/Categories/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCategory(int id)
-        {
-            if (_context.Categories == null)
-            {
-                return NotFound();
-            }
-            var category = await _context.Categories.FindAsync(id);
-            if (category == null)
-            {
-                return NotFound();
-            }
+        //// DELETE: api/Categories/5
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteCategory(int id)
+        //{
+        //    if (_context.Categories == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    var category = await _context.Categories.FindAsync(id);
+        //    if (category == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            _context.Categories.Remove(category);
-            await _context.SaveChangesAsync();
+        //    _context.Categories.Remove(category);
+        //    await _context.SaveChangesAsync();
 
-            return NoContent();
-        }
+        //    return NoContent();
+        //}
 
-        private bool CategoryExists(int id)
-        {
-            return (_context.Categories?.Any(e => e.CategoryId == id)).GetValueOrDefault();
-        }
+        //private bool CategoryExists(int id)
+        //{
+        //    return (_context.Categories?.Any(e => e.CategoryId == id)).GetValueOrDefault();
+        //}
     }
 }
