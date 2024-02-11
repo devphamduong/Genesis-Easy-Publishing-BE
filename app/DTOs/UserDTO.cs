56c02350193df03fcf0da1a85967f565428e98bf
Nguyen Duy Nam [K16_HL] <105804022+namnd78@users.noreply.github.com>
2024-02-12 05:03:53 +0700

    update

---
+++ app/DTOs/UserDTO.cs
@@ -0,0 +1,26 @@
+﻿namespace app.DTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Username { get; set; }
+
+        public string? FullName { get; set; }
+
+        public string? Gender { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? Dob { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? UserImage { get; set; }
+
+        public int? Status { get; set; }
+        //public string? Role { get; set; }
+    }
+}
