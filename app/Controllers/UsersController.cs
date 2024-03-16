298c13872619321074ac72d773670d6741ae5ee3
Nguyen Duy Nam [K16_HL] <105804022+namnd78@users.noreply.github.com>
2024-03-16 12:41:09 +0700

    no message

--- app/Controllers/UsersController.cs
+++ app/Controllers/UsersController.cs
@@ -10,7 +10,7 @@ using app.Service;
 
 namespace app.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class UsersController : ControllerBase
     {
