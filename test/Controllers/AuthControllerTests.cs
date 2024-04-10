e3fe2f8e25e96775a386d5a52418d4d4a4c0f8d8
nguyennq26062001 <nguyennq26062001@gmail.com>
2024-04-11 00:16:55 +0700

    Add File Service and controller Test

---
+++ test/Controllers/AuthControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using app.Controllers;
+using app.Models;
+using app.Service;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static app.Controllers.AuthController;
+
+namespace Genesis_Easy_Publishing_BE.Tests.Controllers
+{
+
+    public class AuthControllerTests 
+    {
+        private readonly EasyPublishingContext _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthControllerTests()
+        {
+            _context = new  EasyPublishingContext();
+            _configuration = A.Fake<IConfiguration>();
+        }
+      
+
+
+    }
+}
---
+++ test/Service/HashService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genesis_Easy_Publishing_BE.Tests.Service
+{
+    internal class HashService
+    {
+    }
+}
